5df2e1cb93fb6af0dc0d7d886f29fca44d236897
sagar singh <sagarsinghkoranga@gmail.com>
2026-01-28 17:49:54 +0530

    path change2

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
    origin: [
     'http://localhost:5173', // Frontend 1
     'http://localhost:5174',
-   'https://ecommerce-admin-git-main-sagarkorangas-projects.vercel.app',
+   'https://ecommerce-admin-rosy-theta.vercel.app',
     'https://ecommerce-frontend-git-main-sagarkorangas-projects.vercel.app', // Frontend 2
   ], // your frontend
     credentials: true,
