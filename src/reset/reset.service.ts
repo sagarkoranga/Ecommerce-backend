7c4958a45b6654c5d6caffe1fc744b206fe96540
sagar singh <sagarsinghkoranga@gmail.com>
2026-01-28 11:49:01 +0530

    reset change

--- src/reset/reset.service.ts
+++ src/reset/reset.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as nodemailer from 'nodemailer';
- import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class ResetService  {
-  constructor(private prisma: PrismaService) {}
+export class ResetService {
+  constructor(private prisma: PrismaService) { }
 
   async forgotPassword(username: string) {
     const admin = await this.prisma.admin.findUnique({ where: { username } });
@@ -25,12 +25,12 @@ export class ResetService  {
       },
     });
 
-    
+
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-        host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: false,
+      host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS,
@@ -45,60 +45,60 @@ export class ResetService  {
 
     return { message: 'OTP sent to email' };
   }
-async verifyOtp(username: string, otphash: string) {
-  const record = await this.prisma.password.findFirst({
-    where: { username },
-    orderBy: { createdAt: 'desc' },
-  });
+  async verifyOtp(username: string, otphash: string) {
+    const record = await this.prisma.password.findFirst({
+      where: { username },
+      orderBy: { createdAt: 'desc' },
+    });
 
-  if (!record) throw new Error('OTP not found');
-  if (record.expiresAt < new Date()) throw new Error('OTP expired');
-  console.log("OTP from frontend:", otphash);
-console.log("OTP as string:", String(otphash));
-console.log("OTP length:", String(otphash).length);
-console.log("DB hash:", record.otp);
+    if (!record) throw new Error('OTP not found');
+    if (record.expiresAt < new Date()) throw new Error('OTP expired');
+    console.log("OTP from frontend:", otphash);
+    console.log("OTP as string:", String(otphash));
+    console.log("OTP length:", String(otphash).length);
+    console.log("DB hash:", record.otp);
 
-  const isValid = await bcrypt.compare(String(otphash), record.otp);
-  console.log(">>>>",isValid)
-  if (!isValid) throw new Error('Invalid OTP');
+    const isValid = await bcrypt.compare(String(otphash), record.otp);
+    console.log(">>>>", isValid)
+    if (!isValid) throw new Error('Invalid OTP');
 
-  const otpToken = randomUUID();
+    const otpToken = randomUUID();
 
-  await this.prisma.password.update({
-    where: { id: record.id },
-    data: { otpToken },
-  });
+    await this.prisma.password.update({
+      where: { id: record.id },
+      data: { otpToken },
+    });
 
-  return { otpToken };
-}
+    return { otpToken };
+  }
 
-async resetPassword(
-  username: string,
-  otpToken: string,
-  newPassword: string,
-) {
-  const record = await this.prisma.password.findFirst({
-    where: {
-      username,
-      otpToken,
-    },
-  });
+  async resetPassword(
+    username: string,
+    otpToken: string,
+    newPassword: string,
+  ) {
+    const record = await this.prisma.password.findFirst({
+      where: {
+        username,
+        otpToken,
+      },
+    });
 
-  if (!record) throw new Error('Unauthorized access');
+    if (!record) throw new Error('Unauthorized access');
 
-  if (record.expiresAt < new Date())
-    throw new Error('Session expired');
+    if (record.expiresAt < new Date())
+      throw new Error('Session expired');
 
-  const hashed = await bcrypt.hash(newPassword, 10);
+    const hashed = await bcrypt.hash(newPassword, 10);
 
-  await this.prisma.admin.update({
-    where: { username },
-    data: { password: hashed },
-  });
+    await this.prisma.admin.update({
+      where: { username },
+      data: { password: hashed },
+    });
 
-  
-  await this.prisma.password.deleteMany({ where: { username } });
 
-  return { message: 'Password reset successful' };
-}
+    await this.prisma.password.deleteMany({ where: { username } });
+
+    return { message: 'Password reset successful' };
+  }
 }
